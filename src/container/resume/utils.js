97c02714295cf01a394677f63d155da954ec915a
RobertoRadina <robertoradina@gmail.com>
2023-08-29 15:17:32 +0200

    alles neu

---
+++ src/container/resume/utils.js
@@ -0,0 +1,48 @@
+export const data = {
+  experience: [
+    {
+      title: "Full Stack Developer",
+      subTitle: "Berlin, Germany",
+      description: " Further training as a web and software developer",
+      date: "2020 - 2021",
+    },
+    {
+      title: "Caretaker",
+      subTitle: "Nürnberg, Germany",
+      description: "Employed to look after a retirement home",
+      date: "2020 - 2021",
+    },
+    {
+      title: "Key Account Manager",
+      subTitle: "Forli, Italy",
+      description: "Foreign sales representative",
+      date: "2020 - 2021",
+    },
+    {
+      title: "Self-employed entrepreneur",
+      subTitle: "Nürnberg, Germany",
+      description: "Pizza shop",
+      date: "2020 - 2021",
+    },
+  ],
+  education: [
+    {
+      title: "Further training as a web and software developer",
+      subTitle: "DCI Digital Career Institute",
+      description: "Full Stack Developer",
+      date: "2020 - 2021",
+    },
+    {
+      title: "Higher Secondary",
+      subTitle: "ABC School of higher education",
+      description: "12th Grade in Science",
+      date: "2020 - 2021",
+    },
+    {
+      title: "B.Tech",
+      subTitle: "XYZ institute of technology",
+      description: "Bachelor of technology in computer science",
+      date: "2020 - 2021",
+    },
+  ],
+};
---
+++ src/container/skills/utils.js
@@ -0,0 +1,74 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "60",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "NODE JS",
+        percentage: "50",
+      },
+      {
+        skillName: "REST API",
+        percentage: "50",
+      },
+    ],
+  },
+  {
+    label: "DATABASES",
+    data: [
+      {
+        skillName: "SQL",
+        percentage: "60",
+      },
+      {
+        skillName: "MONGO DB",
+        percentage: "65",
+      },
+    ],
+  },
+  {
+    label: "Languages",
+    data: [
+      {
+        skillName: "German",
+        percentage: "100",
+      },
+      {
+        skillName: "Italien",
+        percentage: "100",
+      },
+      {
+        skillName: "Venetian",
+        percentage: "95",
+      },
+      {
+        skillName: "Franconian",
+        percentage: "90",
+      },
+      {
+        skillName: "English",
+        percentage: "85",
+      },
+      {
+        skillName: "Bulgarian",
+        percentage: "30",
+      },
+    ],
+  },
+];
